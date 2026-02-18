75df841e0b4235793b0cbc349f87181a8b475364
Bryan Apellanes <bryan.apellanes@gmail.com>
2026-02-18 12:58:22 -0600

    extract presentation types to bam.presentation

--- bamsvc/Presentation/IHtmlPage.cs
+++
@@ -1,9 +0,0 @@
-namespace Bam.Presentation;
-
-public interface IHtmlPage
-{
-    string Path { get; }
-    string Title { get; }
-    string ContentType => "text/html";
-    string Render(object? model = null);
-}
